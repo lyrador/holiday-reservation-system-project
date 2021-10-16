1115c9336d1bba30044406ff7bd9c1a6079dc2e2
raihan <raihan.yusri@gmail.com>
2021-10-16 15:25:34 +0800

    Added Room, RoomRate and RoomType entity classes

---
+++ HolidayReservationSystemProjectLibrary/src/entity/Room.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Room implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roomId;
+    private Long roomNumber;
+    private Boolean roomAvailability;
+    
+    @ManyToOne // owning side
+    private RoomType roomType;
+
+    public Room() {
+    }
+
+    public Room(Long roomNumber, Boolean roomAvailability, RoomType roomType) {
+        this.roomNumber = roomNumber;
+        this.roomAvailability = roomAvailability;
+        this.roomType = roomType;
+    }
+    
+    
+
+    public Long getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(Long roomId) {
+        this.roomId = roomId;
+    }
+
+    public Long getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Long roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Boolean getRoomAvailability() {
+        return roomAvailability;
+    }
+
+    public void setRoomAvailability(Boolean roomAvailability) {
+        this.roomAvailability = roomAvailability;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (roomId != null ? roomId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the roomId fields are not set
+        if (!(object instanceof Room)) {
+            return false;
+        }
+        Room other = (Room) object;
+        if ((this.roomId == null && other.roomId != null) || (this.roomId != null && !this.roomId.equals(other.roomId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Room[ id=" + roomId + " ]";
+    }
+    
+}
